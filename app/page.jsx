bcf1ec82ae49bb2e19d4f456752c40e6a57b5a25
Rock_Katjapon <42570822+barock075282130@users.noreply.github.com>
2023-12-19 17:40:08 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,13 @@
+'use client';
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter()
+  useEffect(()=>{
+    router.push('/products')
+  },[])
   return (
     <div className="pt-16">
       <div className='flex justify-center items-center p-4'>
